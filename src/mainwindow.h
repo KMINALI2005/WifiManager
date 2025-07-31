627eaf3850b77934ca249e57b85a76562fbc1d8e
KMINALI2005 <162717637+KMINALI2005@users.noreply.github.com>
2025-08-01 00:50:33 +0300

    Update mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <memory>
 #include "wifimanager.h"
+#include "networkstats.h"
 
 QT_BEGIN_NAMESPACE
 class QListWidget;
@@ -11,8 +12,11 @@ class QLabel;
 class QPushButton;
 class QLineEdit;
 class QTableWidget;
-class QChartView;
 class QTextEdit;
+class QProgressBar;
+class QChartView;
+class QChart;
+class QPieSeries;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
@@ -32,14 +36,19 @@ private slots:
     void onRefreshClicked();
     void updateNetworkInfo();
     void showDeviceDetails(int row, int column);
+    void onStatsUpdated(const NetworkStats &stats);
+    void checkSystemRequirements();
 
 private:
     void setupUi();
     void createMenuBar();
+    void createChart();
     void updateDeviceTable(const std::vector<Device> &devices);
+    void updateChart(const NetworkStats &stats);
     void showMessage(const QString &message, bool isError = false);
     
     std::unique_ptr<WifiManager> m_wifiManager;
+    std::unique_ptr<NetworkStatsManager> m_statsManager;
     
     // UI Elements
     QTableWidget *m_deviceTable;
@@ -47,11 +56,16 @@ private:
     QLabel *m_signalLabel;
     QLabel *m_devicesCountLabel;
     QLabel *m_bandwidthLabel;
+    QLabel *m_downloadSpeedLabel;
+    QLabel *m_uploadSpeedLabel;
     QPushButton *m_blockBtn;
     QPushButton *m_unblockBtn;
     QPushButton *m_refreshBtn;
     QTextEdit *m_logWidget;
     QChartView *m_chartView;
+    QChart *m_chart;
+    QPieSeries *m_pieSeries;
+    QProgressBar *m_signalProgressBar;
 };
 
 #endif // MAINWINDOW_H
